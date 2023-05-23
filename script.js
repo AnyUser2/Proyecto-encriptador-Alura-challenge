9e7799397e6688f1506fa9c237e0ce79b5231397
Jeronimo <jeropauli@hotmail.com>
2023-05-22 20:48:38 -0500

    Enlace con JAVASCRIPT, creacion de las funcionalidades encriptar y desencriptar, adicion del boton copiar y cambios esteticos

---
+++ script.js
@@ -0,0 +1,36 @@
+function encriptar(){
+    if(document.getElementById("input_mensaje").value.trim().length==0){
+    document.getElementById("impresion").style.display = "none";
+    document.getElementById("copiar").style.display = "none";  
+    }else{
+    var texto = document.getElementById("input_mensaje").value.toLowerCase();
+    var mensaje = texto.replace(/e/igm,"enter");
+    var mensaje = mensaje.replace(/o/igm,"ober");
+    var mensaje = mensaje.replace(/i/igm,"imes");
+    var mensaje = mensaje.replace(/a/igm,"ai");
+    var mensaje = mensaje.replace(/u/igm,"ufat");
+    document.getElementById("impresion").style.display = "inline-block";
+    document.getElementById("copiar").style.display = "inline-block";
+    document.getElementById("impresion").innerHTML = mensaje;
+    console.log(mensaje)
+    }
+    document.getElementById("input_mensaje").value = null;
+}
+function desencriptar(){
+    if(document.getElementById("input_mensaje").value.trim().length==0){
+    document.getElementById("impresion").style.display = "none";
+    document.getElementById("copiar").style.display = "none";
+    }else{
+    var texto = document.getElementById("input_mensaje").value.toLowerCase();
+    var mensaje = texto.replace(/enter/igm,"e");
+    var mensaje = mensaje.replace(/ober/igm,"o");
+    var mensaje = mensaje.replace(/imes/igm,"i");
+    var mensaje = mensaje.replace(/ai/igm,"a");
+    var mensaje = mensaje.replace(/ufat/igm,"u");
+    document.getElementById("impresion").style.display = "inline-block";
+    document.getElementById("copiar").style.display = "inline-block";
+    document.getElementById("impresion").innerHTML = mensaje;
+    console.log(mensaje)
+    }
+    document.getElementById("input_mensaje").value = null;
+}
